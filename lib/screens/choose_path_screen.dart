b07f54a70cbc7725f4721c2b02816724fd0184a3
Bhanu Hasaranga <hasaranga20139@gmail.com>
2024-01-29 22:53:09 +0530

    ui done

--- lib/screens/choose_path_screen.dart
+++ lib/screens/choose_path_screen.dart
@@ -5,8 +5,153 @@ class ChoosePathScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0),
+                child: Text(
+                  'Choose Your Path: Empowering Farmers Together!',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF373737),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 5.0),
+                child: Text(
+                  'Pick your preference and unlock the power of AgroConnect. Join the community to connect, share, and solve farming challenges, or discover the latest agriculture news. Your journey begins here!',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5.0),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<OutlinedBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                    ),
+                    // backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ChoosePathScreen()),
+                    );
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 5.0),
+                          child: Text(
+                            'Join Communtiy',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              color: Color(0xFF4E7D4C),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 5.0),
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                width: 200,
+                                child: Text(
+                                  'Connect, share, and solve farming challenges with fellow farmers. Together, we find solutions and cultivate success.',
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    color: Color(0xFF373737),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<OutlinedBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                    ),
+                    // backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ChoosePathScreen()),
+                    );
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 5.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 5.0),
+                          child: Text(
+                            'Discover News',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              color: Color(0xFF4E7D4C),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 5.0),
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                width: 200,
+                                child: Text(
+                                  'Stay updated with the latest agriculture news, trends, and insights. Empower your farming journey with knowledge and innovation',
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    color: Color(0xFF373737),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
-
 }
