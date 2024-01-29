6a70b3da745e4f068728d3b60d4c68f92a75c754
Bhanu Hasaranga <hasaranga20139@gmail.com>
2024-01-29 14:13:18 +0530

    add padding

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -13,64 +13,76 @@ class StartScreen extends StatelessWidget {
       ),
       bottomSheet: SizedBox(
         width: double.infinity,
-        height: (MediaQuery.of(context).size.height) / 3,
+        height: (MediaQuery.of(context).size.height) / 2.5,
         child: Padding(
-          padding: const EdgeInsets.all(0.0),
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
           child: Column(
             children: [
-              Image.asset(
-                'lib/assets/logo.png',
-                width: (MediaQuery.of(context).size.width) / 2,
-              ),
-              const Text(
-                'Welcome to AgroConnect, where farming knowledge grows together! Connect, share, and thrive with fellow farmers in your community. ',
-                textAlign: TextAlign.center,
-              ),
-              SearchBar(
-                padding: const MaterialStatePropertyAll<EdgeInsets>(
-                  EdgeInsets.symmetric(horizontal: 16.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Image.asset(
+                  'lib/assets/logo.png',
+                  width: (MediaQuery.of(context).size.width) / 2,
                 ),
-                shape: MaterialStateProperty.all<OutlinedBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                    side:
-                        const BorderSide(color: Color(0xFF4E7D4C), width: 2.0),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 5.0),
+                child: Text(
+                  'Welcome to AgroConnect, where farming knowledge grows together! Connect, share, and thrive with fellow farmers in your community. ',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5.0),
+                child: SearchBar(
+                  padding: const MaterialStatePropertyAll<EdgeInsets>(
+                    EdgeInsets.symmetric(horizontal: 16.0),
                   ),
+                  shape: MaterialStateProperty.all<OutlinedBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                      side: const BorderSide(
+                          color: Color(0xFF4E7D4C), width: 2.0),
+                    ),
+                  ),
+                  hintText: 'Search Something',
+                  leading: const Icon(
+                    Icons.search,
+                    color: Color(0xFF4E7D4C),
+                  ),
+                  elevation: MaterialStateProperty.all<double>(0.0),
                 ),
-                hintText: 'Search Something', 
-                leading: const Icon(
-                  Icons.search,
-                  color: Color(0xFF4E7D4C),
-                ),
-                elevation: MaterialStateProperty.all<double>(0.0),
               ),
               const Text('Or'),
-              SizedBox(
-                width: 400,
-                height: 50,
-                child: FilledButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ChoosePathScreen()),
-                    );
-                  },
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<OutlinedBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5.0),
+                child: SizedBox(
+                  width: 400,
+                  height: 50,
+                  child: FilledButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ChoosePathScreen()),
+                      );
+                    },
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<OutlinedBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
                       ),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Color(0xFF4E7D4C)),
                     ),
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Color(0xFF4E7D4C)),
-                  ),
-                  child: const Text(
-                    'Get Started',
-                    style: TextStyle(
-                      fontSize: 19,
-                      fontWeight: FontWeight.w600,
-                      letterSpacing: 0.19,
+                    child: const Text(
+                      'Get Started',
+                      style: TextStyle(
+                        fontSize: 19,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 0.19,
+                      ),
                     ),
                   ),
                 ),
