fc4b23188161cf3c284e77928a43f6599dcb6632
Bhanu Hasaranga <hasaranga20139@gmail.com>
2024-03-02 10:55:54 +0530

    landing screen

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/contentCategories.dart';
+import '../widgets/navBar.dart';
+import '../widgets/newsCard.dart';
+import '../widgets/postSomethingField.dart';
+
 class LandingScreen extends StatelessWidget {
   const LandingScreen({Key? key}) : super(key: key);
   @override
@@ -19,17 +24,48 @@ class LandingScreen extends StatelessWidget {
           IconButton(
             onPressed: () {},
             icon: const Icon(Icons.search),
+            iconSize: 35,
           ),
           IconButton(
             onPressed: () {},
-            icon: const Icon(Icons.notifications),
+            icon: const Icon(Icons.notifications_outlined),
+            iconSize: 35,
           ),
           IconButton(
             onPressed: () {},
             icon: const Icon(Icons.menu),
+            iconSize: 35,
           )
         ],
       ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        child: Column(
+          children: [
+            PostSomethingField(
+              lableText: 'Post a News',
+            ),
+            ContentCategories(
+              category1: 'Latest',
+              category2: 'Trending',
+              category3: 'All',
+            ),
+            SizedBox(
+              height: 490,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: [
+                    NewsCard(),
+                    NewsCard(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: const NavBar(),
     );
   }
 }
