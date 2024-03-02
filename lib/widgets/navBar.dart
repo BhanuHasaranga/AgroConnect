6ad659fe152e86263b384a47663019ffab7549cb
Bhanu Hasaranga <hasaranga20139@gmail.com>
2024-03-02 11:27:45 +0530

    navBar routs

--- lib/widgets/navBar.dart
+++ lib/widgets/navBar.dart
@@ -1,4 +1,7 @@
 import 'package:agro_connect/screens/ask_screen.dart';
+import 'package:agro_connect/screens/landing_screen.dart';
+import 'package:agro_connect/screens/saved_content_screen.dart';
+import 'package:agro_connect/screens/user_profile_screen.dart';
 import 'package:flutter/material.dart';
 
 class NavBar extends StatelessWidget {
@@ -12,25 +15,41 @@ class NavBar extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const LandingScreen()),
+            );
+          },
           icon: const Icon(Icons.home_outlined),
         ),
         IconButton(
           onPressed: () {
-                                Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const AskScreen()),
-                    );
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const AskScreen()),
+            );
           },
           icon: const Icon(Icons.chat_outlined),
         ),
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const SavedContentScreen()),
+            );
+          },
           icon: const Icon(Icons.bookmarks_outlined),
         ),
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const UserProfileScreen()),
+            );
+          },
           icon: const CircleAvatar(
             radius: 15,
             backgroundImage: AssetImage(
