28ab828630ab382ccbbc0c6efbd380cd9516379d
Bhanu Hasaranga <hasaranga20139@gmail.com>
2024-03-19 14:40:34 +0530

    image picker done

--- lib/widgets/imagePicker.dart
+++ lib/widgets/imagePicker.dart
@@ -1,61 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-class CustomImagePicker extends StatefulWidget {
+class CustomImagePicker extends StatelessWidget {
   final Function(XFile?) onImageSelected;
 
   const CustomImagePicker({Key? key, required this.onImageSelected})
       : super(key: key);
 
   @override
-  State<CustomImagePicker> createState() => _CustomImagePickerState();
-
-  // Method to show image picker dialog
-  static Future<void> showImagePickerDialog(BuildContext context, Function(XFile?) onImageSelected) async {
-    return showDialog<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          alignment: Alignment.bottomCenter,
-          content: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              IconButton(
-                onPressed: () async {
-                  XFile? image = await ImagePicker().pickImage(source: ImageSource.camera);
-                  onImageSelected(image);
-                  Navigator.of(context).pop();
-                },
-                icon: Icon(
-                  Icons.camera_alt_outlined,
-                  color: Color(0x7F373737),
-                  size: 30,
-                ),
-              ),
-              IconButton(
-                onPressed: () async {
-                  XFile? image = await ImagePicker().pickImage(source: ImageSource.gallery);
-                  onImageSelected(image);
-                  Navigator.of(context).pop();
-                },
-                icon: Icon(
-                  Icons.photo_outlined,
-                  color: Color(0x7F373737),
-                  size: 30,
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-}
-
-class _CustomImagePickerState extends State<CustomImagePicker> {
-  @override
   Widget build(BuildContext context) {
-    // We don't need to put any logic here
     return SizedBox.shrink();
   }
+
+  static const Color iconColor = Color(0x7F373737);
+
+  static Future<XFile?> showImagePickerDialog(BuildContext context) async {
+  return showDialog<XFile?>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        alignment: Alignment.bottomCenter,
+        contentPadding: EdgeInsets.all(16),
+        content: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              onPressed: () async {
+                XFile? image = await ImagePicker().pickImage(source: ImageSource.camera);
+                Navigator.of(context).pop(image);
+              },
+              icon: Icon(
+                Icons.camera_alt_outlined,
+                color: iconColor,
+                size: 30,
+              ),
+            ),
+            IconButton(
+              onPressed: () async {
+                XFile? image = await ImagePicker().pickImage(source: ImageSource.gallery);
+                Navigator.of(context).pop(image);
+              },
+              icon: Icon(
+                Icons.photo_outlined,
+                color: iconColor,
+                size: 30,
+              ),
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
+
 }
