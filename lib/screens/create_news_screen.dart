091bb344aa4bf511410cb8d1e88b242629eb9746
Bhanu Hasaranga <hasaranga20139@gmail.com>
2024-03-19 14:40:58 +0530

    create news screen done

--- lib/screens/create_news_screen.dart
+++ lib/screens/create_news_screen.dart
@@ -1,11 +1,9 @@
-import 'dart:io';
+import '../widgets/imagePicker.dart';
 
-import 'package:dotted_border/dotted_border.dart';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-
-import '../widgets/imagePicker.dart';
-// import 'package:image_picker/image_picker.dart';
+import 'package:dotted_border/dotted_border.dart';
 
 class CreateNews extends StatefulWidget {
   const CreateNews({Key? key}) : super(key: key);
@@ -17,7 +15,7 @@ class CreateNews extends StatefulWidget {
 class _CreateNewsState extends State<CreateNews> {
   String headline = "";
   String news = "";
-  XFile? image_0;
+  List<XFile?> selectedImages = [];
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +28,6 @@ class _CreateNewsState extends State<CreateNews> {
             fontSize: 25,
             fontFamily: 'Poppins',
             fontWeight: FontWeight.w600,
-            height: 0,
           ),
         ),
       ),
@@ -38,203 +35,194 @@ class _CreateNewsState extends State<CreateNews> {
         padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
         child: Column(
           children: [
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const CircleAvatar(
-                    radius: 15,
-                    backgroundImage: AssetImage(
-                      'lib/assets/user.png',
-                    ),
-                  ),
-                ),
-                const Text(
-                  'Jane Miles',
-                  style: TextStyle(
-                    color: Color(0xFF373737),
-                    fontSize: 15,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w600,
-                    height: 0,
-                    letterSpacing: 0.15,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            // Text(
-            //   headline,
-            //   style: const TextStyle(
-            //     color: Color(0xFF373737),
-            //   ),
-            // ),
-            TextField(
+            _buildUserDetails(),
+            const SizedBox(height: 15),
+            _buildTextField(
+              label: 'Write Your headline here',
               maxLength: 50,
-              onSubmitted: (String str) {
-                setState(() {
-                  headline = str;
-                });
-              },
-              decoration: InputDecoration(
-                contentPadding:
-                    const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                hintText: 'Write Your headline here',
-                hintStyle: const TextStyle(
-                  color: Color(0x7F373737),
-                  fontSize: 14,
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
+              onChanged: (value) => setState(() => headline = value),
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextField(
+            const SizedBox(height: 10),
+            _buildTextField(
+              label: 'Write your News!',
               maxLength: 200,
               maxLines: 5,
-              // minLines: 1,
-              onSubmitted: (String str) {
-                setState(() {
-                  news = str;
-                });
-              },
-              decoration: InputDecoration(
-                contentPadding:
-                    const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                hintText: 'Write your News!',
-                hintStyle: const TextStyle(
-                  color: Color(0x7F373737),
-                  fontSize: 12,
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
+              onChanged: (value) => setState(() => news = value),
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            DottedBorder(
-              // strokeWidth: 1,
-              dashPattern: [8, 8],
-              radius: const Radius.circular(5),
-              color: const Color(0xFF373737),
-              child: GestureDetector(
-                onTap: () {
-                  CustomImagePicker.showImagePickerDialog(context,
-                      (XFile? image) {
-                    setState(() {
-                      image_0 = image;
-                    });
-                  });
-                },
-                child: Container(
-                  height: 100,
-                  child: const Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.add_photo_alternate_outlined,
-                          color: Color(0x7F373737),
-                          size: 30,
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          'Upload Photos/Videos',
-                          style: TextStyle(
-                            color: Color(0x7F373737),
-                            fontSize: 12,
-                            fontFamily: 'Poppins',
-                            fontWeight: FontWeight.w400,
-                            height: 0,
-                            letterSpacing: -0.24,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              children: [
-                image_0 == null
-                    ? const Icon(
-                        Icons.image,
-                        color: Color(0x7F373737),
-                        size: 100,
-                      )
-                    : Image.file(
-                        File(image_0!.path),
-                        width: 75,
-                        height: 75,
-                        fit: BoxFit.fill,
-                      ),
-              ],
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: FilledButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<OutlinedBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                      ),
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color(0xFF4E7D4C)),
-                      fixedSize: MaterialStateProperty.all<Size>(
-                        const Size.fromHeight(
-                            50), // Set the desired height here
-                      ),
-                    ),
-                    child: const Text(
-                      'Post your News',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            const SizedBox(height: 10),
+            _buildImagePicker(),
+            const SizedBox(height: 20),
+            _buildImageGrid(),
+            const SizedBox(height: 20),
+            _buildPostButton(),
           ],
         ),
       ),
     );
   }
-}
 
-// Future selectImage() {
-//   return showDialog(
-//     context: context,
-//     builder: (BuildContext context) {
-//       return Dialog(
-//         child: Text('hi'),
-//       );
-//     },
-//   );
-// }
+  Widget _buildUserDetails() {
+    return Row(
+      children: [
+        IconButton(
+          onPressed: () {},
+          icon: const CircleAvatar(
+            radius: 15,
+            backgroundImage: AssetImage('lib/assets/user.png'),
+          ),
+        ),
+        const Text(
+          'Jane Miles',
+          style: TextStyle(
+            color: Color(0xFF373737),
+            fontSize: 15,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w600,
+            letterSpacing: 0.15,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTextField({
+    required String label,
+    required int maxLength,
+    int maxLines = 1,
+    required ValueChanged<String> onChanged,
+  }) {
+    return TextField(
+      maxLength: maxLength,
+      maxLines: maxLines,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        hintText: label,
+        hintStyle: const TextStyle(
+          color: Color(0x7F373737),
+          fontSize: 14,
+          fontFamily: 'Poppins',
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildImagePicker() {
+    return GestureDetector(
+      onTap: _selectImage,
+      child: DottedBorder(
+        dashPattern: [8, 8],
+        radius: const Radius.circular(5),
+        color: const Color(0xFF373737),
+        child: Container(
+          height: 100,
+          child: const Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.add_photo_alternate_outlined,
+                  color: Color(0x7F373737),
+                  size: 30,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Upload Photos/Videos',
+                  style: TextStyle(
+                    color: Color(0x7F373737),
+                    fontSize: 12,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w400,
+                    letterSpacing: -0.24,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildImageGrid() {
+    return GridView.builder(
+      shrinkWrap: true,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4,
+        crossAxisSpacing: 4.0,
+        mainAxisSpacing: 4.0,
+      ),
+      itemCount: selectedImages.length,
+      itemBuilder: (context, index) {
+        return selectedImages[index] == null
+            ? const Icon(
+                Icons.image,
+                color: Color(0x7F373737),
+                size: 100,
+              )
+            : Image.file(
+                File(selectedImages[index]!.path),
+                width: 75,
+                height: 75,
+                fit: BoxFit.fill,
+              );
+      },
+    );
+  }
+
+  Widget _buildPostButton() {
+    return Row(
+      children: [
+        Expanded(
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              backgroundColor: const Color(0xFF4E7D4C),
+              padding: const EdgeInsets.all(0),
+              fixedSize: const Size.fromHeight(50),
+            ),
+            child: const Text(
+              'Post your News',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _selectImage() async {
+    if (selectedImages.length >= 4) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: const Text('You can\'t select more than 4 images'),
+        ),
+      );
+      return;
+    }
+
+    XFile? image = await CustomImagePicker.showImagePickerDialog(context);
+    if (image != null) {
+      setState(() {
+        selectedImages.add(image);
+        print('Selected Images:');
+        for (var i = 0; i < selectedImages.length; i++) {
+          print(selectedImages[i]?.path);
+        }
+      });
+    }
+  }
+}
