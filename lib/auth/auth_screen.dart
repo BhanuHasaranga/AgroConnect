4e6ffa656781a666d66a8d5fde60319fa9857fab
Bhanu Hasaranga <hasaranga20139@gmail.com>
2024-03-26 01:12:26 +0530

    auth

---
+++ lib/auth/auth_screen.dart
@@ -0,0 +1,28 @@
+import 'package:agro_connect/screens/login_screen.dart';
+import 'package:agro_connect/screens/signup_screen.dart';
+import 'package:flutter/material.dart';
+
+class Auth extends StatefulWidget {
+  const Auth({super.key});
+
+  @override
+  State<Auth> createState() => _AuthState();
+}
+
+class _AuthState extends State<Auth> {
+  bool a = true;
+  void go() {
+    setState(() {
+      a = !a;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (a) {
+      return Login(go);
+    } else {
+      return SignUp(go);
+    }
+  }
+}
