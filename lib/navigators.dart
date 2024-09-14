ea0721dbda69cefc6f45d3d5f370c7ea7fc9cd79
Bhanu Hasaranga <hasaranga20139@gmail.com>
2024-09-14 21:20:57 +0530

    create navigators.dart

---
+++ lib/navigators.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+final GlobalKey<NavigatorState> globalNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'Global');
+final GlobalKey<NavigatorState> landingNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'LandingPage');
+final GlobalKey<NavigatorState> askNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'AskPage');
+final GlobalKey<NavigatorState> savedNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'SavedPage');
+final GlobalKey<NavigatorState> profileNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'ProfilePage');
